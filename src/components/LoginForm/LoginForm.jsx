be14628a7481aff671a2f8bf7d47a98fb9c5ae40
jaycikey <138725941+jaycikey@users.noreply.github.com>
2024-03-03 11:08:39 -0500

    upd

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,21 +1,42 @@
+import { Formik, Form, Field } from "formik";
 import { useDispatch } from "react-redux";
 import { toast } from "react-hot-toast";
 import { logIn } from "../../redux/auth/operations";
 import { TextField, Box, Container, Typography } from "@mui/material";
 import Button from "../MUIButton/MUIButton";
+import * as Yup from "yup";
 
 const LoginForm = () => {
   const dispatch = useDispatch();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const email = e.target.email.value;
-    const password = e.target.password.value;
+  const initialValues = {
+    email: "",
+    password: "",
+  };
 
+  const validationSchema = Yup.object().shape({
+    email: Yup.string()
+      .email("Invalid email address")
+      .required("Required")
+      .test(
+        "is-not-empty",
+        "Email cannot be just spaces",
+        (value) => value?.trim().length > 0
+      ),
+    password: Yup.string()
+      .required("Required")
+      .test(
+        "is-not-empty",
+        "Password cannot be just spaces",
+        (value) => value?.trim().length > 0
+      ),
+  });
+
+  const handleSubmit = async (values, { resetForm }) => {
     try {
-      await dispatch(logIn({ email, password })).unwrap();
+      await dispatch(logIn(values)).unwrap();
       toast.success("Login successful!");
-      e.target.reset();
+      resetForm();
     } catch (error) {
       toast.error("Login failed");
     }
@@ -23,34 +44,46 @@ const LoginForm = () => {
 
   return (
     <Container maxWidth="xs">
-      <form onSubmit={handleSubmit} autoComplete="off">
-        <Box sx={{ my: 2 }}>
-          <Typography variant="h6">Login</Typography>
-        </Box>
-        <Box sx={{ my: 2 }}>
-          <TextField
-            fullWidth
-            label="Email"
-            name="email"
-            type="email"
-            variant="outlined"
-            required
-          />
-        </Box>
-        <Box sx={{ my: 2 }}>
-          <TextField
-            fullWidth
-            label="Password"
-            name="password"
-            type="password"
-            variant="outlined"
-            required
-          />
-        </Box>
-        <Box sx={{ my: 2, display: "flex", justifyContent: "center" }}>
-          <Button type="submit">Log In</Button>
-        </Box>
-      </form>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        {({ isSubmitting }) => (
+          <Form autoComplete="off">
+            <Box sx={{ my: 2 }}>
+              <Typography variant="h6">Login</Typography>
+            </Box>
+            <Box sx={{ my: 2 }}>
+              <Field
+                as={TextField}
+                fullWidth
+                label="Email"
+                name="email"
+                type="email"
+                variant="outlined"
+                required
+              />
+            </Box>
+            <Box sx={{ my: 2 }}>
+              <Field
+                as={TextField}
+                fullWidth
+                label="Password"
+                name="password"
+                type="password"
+                variant="outlined"
+                required
+              />
+            </Box>
+            <Box sx={{ my: 2, display: "flex", justifyContent: "center" }}>
+              <Button type="submit" disabled={isSubmitting}>
+                Log In
+              </Button>
+            </Box>
+          </Form>
+        )}
+      </Formik>
     </Container>
   );
 };
--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,23 +1,50 @@
+import { Formik, Form, Field } from "formik";
 import { useDispatch } from "react-redux";
 import { toast } from "react-hot-toast";
 import { register } from "../../redux/auth/operations";
 import { TextField, Box, Container, Typography } from "@mui/material";
 import Button from "../MUIButton/MUIButton";
+import * as Yup from "yup";
 
 const RegisterForm = () => {
   const dispatch = useDispatch();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const form = e.currentTarget;
-    const name = form.elements.name.value;
-    const email = form.elements.email.value;
-    const password = form.elements.password.value;
+  const initialValues = {
+    name: "",
+    email: "",
+    password: "",
+  };
 
+  const validationSchema = Yup.object().shape({
+    name: Yup.string()
+      .required("Required")
+      .test(
+        "is-not-empty",
+        "Name cannot be just spaces",
+        (value) => value?.trim().length > 0
+      ),
+    email: Yup.string()
+      .email("Invalid email address")
+      .required("Required")
+      .test(
+        "is-not-empty",
+        "Email cannot be just spaces",
+        (value) => value?.trim().length > 0
+      ),
+    password: Yup.string()
+      .required("Required")
+      .test(
+        "is-not-empty",
+        "Password cannot be just spaces",
+        (value) => value?.trim().length > 0
+      ),
+  });
+
+  const handleSubmit = async (values, { resetForm }) => {
     try {
-      await dispatch(register({ name, email, password })).unwrap();
+      await dispatch(register(values)).unwrap();
       toast.success("Registration successful!");
-      form.reset();
+      resetForm();
     } catch (error) {
       toast.error("Registration failed");
     }
@@ -25,43 +52,56 @@ const RegisterForm = () => {
 
   return (
     <Container maxWidth="xs">
-      <form onSubmit={handleSubmit} autoComplete="off">
-        <Box sx={{ my: 2 }}>
-          <Typography variant="h6">Register</Typography>
-        </Box>
-        <Box sx={{ my: 2 }}>
-          <TextField
-            fullWidth
-            label="Username"
-            name="name"
-            variant="outlined"
-            required
-          />
-        </Box>
-        <Box sx={{ my: 2 }}>
-          <TextField
-            fullWidth
-            label="Email"
-            name="email"
-            type="email"
-            variant="outlined"
-            required
-          />
-        </Box>
-        <Box sx={{ my: 2 }}>
-          <TextField
-            fullWidth
-            label="Password"
-            name="password"
-            type="password"
-            variant="outlined"
-            required
-          />
-        </Box>
-        <Box sx={{ my: 2, display: "flex", justifyContent: "center" }}>
-          <Button type="submit">Register</Button>
-        </Box>
-      </form>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        {({ isSubmitting }) => (
+          <Form autoComplete="off">
+            <Box sx={{ my: 2 }}>
+              <Typography variant="h6">Register</Typography>
+            </Box>
+            <Box sx={{ my: 2 }}>
+              <Field
+                as={TextField}
+                fullWidth
+                label="Username"
+                name="name"
+                variant="outlined"
+                required
+              />
+            </Box>
+            <Box sx={{ my: 2 }}>
+              <Field
+                as={TextField}
+                fullWidth
+                label="Email"
+                name="email"
+                type="email"
+                variant="outlined"
+                required
+              />
+            </Box>
+            <Box sx={{ my: 2 }}>
+              <Field
+                as={TextField}
+                fullWidth
+                label="Password"
+                name="password"
+                type="password"
+                variant="outlined"
+                required
+              />
+            </Box>
+            <Box sx={{ my: 2, display: "flex", justifyContent: "center" }}>
+              <Button type="submit" disabled={isSubmitting}>
+                Register
+              </Button>
+            </Box>
+          </Form>
+        )}
+      </Formik>
     </Container>
   );
 };
